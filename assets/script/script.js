5d1a8654c735738ef96e37c63d971c1bb4232321
Vovchik <vova2000ra@mail.ru>
2023-08-09 12:39:21 +0600

    animation

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,3 +4,36 @@ const burgerIcon = document.querySelector('.burger-icon');
 burgerIcon.addEventListener('click', () => {
   burgerMenu.classList.toggle('active');
 });
+
+const header = document.querySelector('.header');
+const headerHeight = header.offsetHeight;
+const main = document.querySelector('.main');
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= headerHeight) {
+    header.classList.add('fixed');
+    main.style.marginTop = `${headerHeight}px`;
+  } else {
+    header.classList.remove('fixed');
+    main.style.marginTop = 0;
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const decoration = document.querySelector(".decoration");
+  const images = decoration.querySelectorAll("img");
+
+  function parallaxScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    for (let i = 0; i < images.length; i++) {
+      const image = images[i];
+      const speed = parseFloat(image.getAttribute("data-speed")) || 0.2;
+      const translateY = -scrollTop * speed;
+
+      image.style.transform = `translate3d(0, ${translateY}px, 0)`;
+    }
+  }
+
+  window.addEventListener("scroll", parallaxScroll);
+  parallaxScroll(); // Вызываем функцию для применения эффекта при загрузке страницы
+});
